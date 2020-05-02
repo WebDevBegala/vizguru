d714681db04b94ff8deb44c72edd67d9c1f32d02
WebDevBegala <begala.g1999@gmail.com>
2020-05-03 01:03:04 +0200

    Bővebben és mobil

---
+++ script/data.js
@@ -0,0 +1,42 @@
+let more1 = `Próbálsz egészségesen élni: sportos, fitten eszel és tudod, hogy a tiszta víz milyen fontos: ezért munka és sport közben is sokat iszol.
+Tisztában vagy vele, hogy a megfelelő mennyiségű és minőségű víz fogyasztása jót tesz a szépségednek és  még frissebb is leszel tőle. Pláne a jelenlegi helyzetben, amikor alig mehetsz ki, még fontosabb a megfelelő mennyiségű és minőségű víz.
+Elmenni a boltba, haza cipelni...most veszélyes is lehet - főleg a szüleid számára - ebben a koronás helyzetben.
+Tudd meg hogyan legyen super és egészséges ivóvized otthon, anélkül, hogy palackos vizet keljen venned. Csak frissen és vidáman.
+`;
+
+let more2 = `Szia Hrasztics Zoltán (Zoli) vagyok. Az egyik szerelmem a víz, 13+ éve egészséges életmódot folytatok, és lassan 30 éve sportolok valamit (7 évesen kezdtem). Imádom a természetet, a nevetést, az ellazult tudatállapotot. Végzettség tekintve diplomás IT Mérnök vagyok, meg marketinges, vezető és víztisztító szakértő is.
+2009 óta kialakult szaktudással foglalkozom víztisztítással, ilyen készülékekkel, sőt volt, amikor víz kutatóval, tenger biológussal dolgoztam együtt 1-1 projektben. Sok könyvet, szakanyagot elolvastam a témában. Ez nekem egy hobbi, és főleg nem ebből élek. Ennek ellenére több ezer elégedett viztisztító vásárló ügyfelem van/volt, a telefonszámom és email címem 2009 óta ugyanaz.
+Én tényleg megszeretném menteni a természetet a műanyag palack szeméttől. A nem tartós, gyorsan eldobható műanyagok használatát egy elhibázott, ostoba döntésnek tartom (persze gazdaságilag racionális döntés volt elkezdeni használni, de a pénz nem termel oxigént és megenni sem lehet).
+`;
+
+let more3 = `Fontos, hogy csak tiszta vizet igyál! A csapvíz klórt és számos egyéb nem kívánatos vegyületet, antibiotikum származékot, vegyszermaradványt...stb. tartalmaz- NE terheld vele a szervezetedet!
+A legtöbb ember palackos vizet iszik, napi 1 palackos víz, az egy évben 365 palack hulladékot is jelenthet, aminek a statisztikák szerint 70%-a kikerül a környezetbe és ez sajnos nem bomlik le, mind szemét lesz! Tudtad, átlagosan percenként 33.800 db műanyag palack kerül csak a Földközi-tengerbe? A többi tengert, óceánt beleszámolva ez iszonyat mennyiség globálisan. Szép lassan vissza hullik a fejünkre ez a “kaki”, 20 év és kb. nem lesz természet. Amihez nagyon is sok köze van a műanyag hulladéknak.
+`;
+
+let more4 = `Gondoltál-e már arra, hogy milyen kiadás a palackos vizek fogyasztása?
+Pénzügyileg gondolj bele, veszel a kondi teremben, boltban, egyéb helyeken is vizet, havi 5000-6000 Ft-ot simán elköltesz palackos vízre, az egy évben legalább 50-60 ezer Ft vagy akár 100ezer Ft is lehet. Pedig egészséges, minőségi, tiszta vizet, ami nem termel műanyag szemetet, ennek a töredékéért is tudsz inni.
+
+Sportos, egészségre törekvő nőként évente 480 db műanyag palackot is kidobhatsz.
+Sportos, belevaló férfiként évente 570 db műanyag palackot is kidobhatunk. Napi átlag 3 l ásványvíz, havi 48 palack, évi 570 db.
+
+Napjainkban az ásványvíz forgalmazás többmiliárd dolláros üzlet. Márkák tucatjai közül választhatunk az élelmiszerüzletekben. Gondoltad volna, hogy ezek közül jó néhány nem több, mint egy kicsit “feljavított” csapvíz?
+`;
+
+let more5 = `Biológusok és környezetvédők évtizedek óta figyelmeztetnek az ásványvíz és üdítős palackok okozta környezetszennyezésre.
+
+Globális szinten a műanyag szemét  91%-át nem hasznosítják újra, EU-ban a 70%-át nem.
+Átlagosan percenként 33.800 db műanyag palack kerül a Földközi-tengerbe...
+ Az ENSZ adatai szerint évente kb. 8 millió tonna műanyag hulladék jut a világ óceánjaiba.Csak hogy jobban el tudjuk képzelni: ez olyan mintha 30 cm-enként  3 bevásárlótáskányi szemetet borítanánk a vizekbe.
+ A Csendes-Óceánon egy fél Európa méretű szemétsziget úszik és a mérete évről-évre egyre nő.
+A műanyag jelenléte a természetes vizekben az egyik legnagyobb veszély a vadvilágra nézve. Rengeteg állat esik a szemét áldozatául: nagy mennyiségű műanyagot nyelnek le, ami végzetes számukra.Ráadásul a műanyag évszázadok alatt sem bomlik le (legfeljebb felaprózódik, szétesik) és alattomosan, számunkra alig észrevehetően, de pusztítóan hat a világunkra. Ha a jelenlegi tendencia folytatódik 2050-re több lesz a műanyag, mint a hal a tengerek, óceánok vizében.
+Hihetetlenül fontos a jó minőségű víz, de kérve kérlek a környezetet NE szennyezzük, totál tönkre mehet az igen közeli jövőnk ettől. A műanyag bele kerül a termőföldekbe, halakba, állatokba és a táplálkozási lánc révén végül vissza kerül belénk, emberekbe is.
+`;
+
+let more6 = `Hazánk kiváló minőségű ivóvíz készlettel rendelkezik, 25%-ban karszt víz (lényegében ásványvíz), 35%-ban mélységi, védett rétegű víz (lényegében 90%-os ásványvíz), 35% folyamparti kavicságy (lényegében 50%-os ásványvíz). De klórozzák.
+Klóros vizet inni hosszú távon egészségtelen. A klórgázt az első világháborúban idegméregként használták, de erős baktérium és vírusölő hatása is van. Az a baj, hogy nemcsak a vizet fertőtleníti, hanem szervezetedet is: elpusztítja például a hasznos bélflórát. Mivel az egészségügyi hatóságok szerint a klórnak jelen kell lennie az ivóvízben, így fogyasztás, felhasználás előtt magunknak kell eltávolítanunk belőle. Ezt sem forralással, sem párologtatással nem lehet megtenni, ez egy buta tévhit.
+A régi építésű lakóházakban ólom csővezetékek lehetnek. A vízmű hálózatban már nincsenek ólom csövek, évekkel ezelőtt befejezte a Vízmű ezek teljes cseréjét. Ám a lakóházon belül senkinek se feladata hivatalból ezeket kicserélni. Az ólom vezetékből kis koncentrációban ólom juthat az ivóvízbe, ami hosszú távon káros.
+
+A jó hír, hogy ezeket kivenni egyszerűbb, mint gondolnád egy aktív szenes víztisztítóval. Vállalj felelősséget az egészségedért és használj Super Aqua vízszűrőt a konyhádba!. Ezzel már sokat teszel az egészségedért, ráadásul a víz ízletesebb lesz, és a kellemetlen klór szagot sem kell többé elviselned, és ólom mentes lesz a vized.
+`;
+
+let moreTexts = [more1,more2,more3,more4,more5,more6];
