d93938d51d8c68bcf8f13bf2a275241dd80ee866
WebDevBegala <begala.g1999@gmail.com>
2020-06-13 12:14:11 +0200

    +videó,képek

---
+++ script/style.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+   console.log("Ready")
+   if ($(document).width() <= 500) {
+      console.log("Mobil")
+      $(".content7 > .left").css("height")
+      $("p").css("font-size", "3.5vw")
+      $(".list-icon").css("font-size", "4vw");
+      $(".list-icon-incorrect").css("font-size","4vw");
+      $("p").css("font-size", "4vw");
+   }
+})
+
+
+$(".btn").click((event) => {
+   let currentDia = $(event.target).closest(".dia").attr("class");
+  let index = getNumFromClass(currentDia)
+  $(".more-text").text(moreTexts[index-1])
+   $(".more-block").fadeIn();
+
+})
+$(".gg-close-r").click(() => {
+   $(".more-block").fadeOut()
+})
+
+function getNumFromClass(text){
+   let newText = text.split(" ");
+   let number = newText[1].substr(3);
+   return number
+
+}
+
+function goToBuy(){
+   window.location.href = 'megrendeles.html';
+}
